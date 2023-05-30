3186390442feee7bb07b966f60f9a82d64175603
Petru <petrupascari1994@gmail.com>
2023-05-30 17:45:05 +0300

    Added the implementation of checked and unchecked exceptions

---
+++ src/main/java/com/petrupascari/exceptionsservicetask/ArithmeticOperationsService.java
@@ -0,0 +1,20 @@
+package com.petrupascari.exceptionsservicetask;
+
+public class ArithmeticOperationsService {
+    public static int divideIntNoExceptionHandling(int a, int b) {
+        return a / b;
+
+    }
+
+    public static int divideIntWithExceptionHandling(int a, int b) {
+        try {
+            int result = a / b;
+            return result;
+        } catch (Exception capturedExeption) {
+            System.out.println("An exeption has occured: " + capturedExeption.getMessage());
+            return 0;
+        }
+
+
+    }
+}
